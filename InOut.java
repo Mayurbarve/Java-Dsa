b7cc65400569a854033b46590e2322ec2fa345de
Mayurbarve <mayurbarve04@gmail.com>
2023-12-13 12:22:43 +0530

    new file add inout

---
+++ InOut.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class In_out{
+    public static void main(String[] args){
+        //for output
+        System.out.println("first github repository");
+
+        // for Taking input
+        Scanner in = new Scanner(System.in);
+
+        //object for input
+        int obj = in.nextInt();
+
+        System.out.println("integar value:"+ obj);
+
+    }
+}
