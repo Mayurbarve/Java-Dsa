3034f422eee744476034f5ad9be10125996b8ca5
Mayura Dudhbarve <mayurbarve04@gmail.com>
2024-03-22 23:53:39 +0530

    loop modifiled

---
+++ Loop.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Loop{
+    public static void main(String[] args){
+    //While loop
+    int count = 1;
+    while(count != 5){
+        Sytstem.out.println(count);
+        count++;
+    }
+    //for loop
+    for(int count=1;  count != 5; count++){
+        System.out.println(count);
+    }
+    //do while loop
+    int num1=1;
+    do{
+        System.out.println(num);
+        num++;
+    }
+    while(n!=1);
+    }
+}
