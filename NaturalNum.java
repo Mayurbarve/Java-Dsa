fbb59c52c6a26fa9c9167f5c5c008b6b46fd6a4a
Mayurbarve <mayurbarve04@gmail.com>
2024-07-11 17:23:34 +0530

    New Files

---
+++ NaturalNum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NaturalNum {
+    public static void main(String args[]) {
+        System.out.println("Natural Numbers");
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the N Number: ");
+        int Nnum = in.nextInt();
+        in.close();
+
+        for (int i = 0; i <= Nnum; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
