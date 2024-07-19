7ff8d605c19289e5881a24738f72af95330318f7
Mayurbarve <mayurbarve04@gmail.com>
2024-07-19 14:56:49 +0530

    Linear Search

--- LinearSearch/EvenDigits.java
+++ LinearSearch/EvenDigits.java
@@ -1,14 +1,12 @@
 //Return how many of them contain an even number of digits
 
-public class Demo {
+public class EvenDigits {
     public static void main(String[] args) {
         int[] nums = {12, 345, 2, 6, 7896};
         System.out.println(findNumber(nums));
-
-        System.out.println(digits(345));
     }
 
-    static int findNumber(int[] nums) {
+    public static int findNumber(int[] nums) {
         int count = 0;
         for (int num : nums) {
             if (even(num)) {
@@ -19,7 +17,7 @@ public class Demo {
     }
 
     // function to check whether a number contains even digit or not
-    static boolean even(int num) {
+    public static boolean even(int num) {
         int numberOfDigits = digits(num);
         if(numberOfDigits % 2 == 0){
             return true;
@@ -28,25 +26,14 @@ public class Demo {
         // return numberOfDigits % 2 ==0;
     }
 
-    // function to check the digit of a number
-    static int digits(int num) {
-        if(num <0){
-            num = num * -1;
+    public static int digits(int num) {
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num /= 10;
         }
 
-        // if(num == 0){
-        //     return 1;
-        // }
-
-        // int count = 0;
-        // while (num > 0) {
-        //     count++;
-        //     num /= 10;
-        // }
-
-        // return count;
-
-        return (int)(Math.log10(num) + 1);
+        return count;
     }
 
 }
