3593cc1d2619f298584403cf5d6f6679007fe66c
Mayurbarve <mayurbarve04@gmail.com>
2024-07-27 16:53:51 +0530

    Updated

--- CodingQuestions/OccurrenceOfElement.java
+++ CodingQuestions/OccurrenceOfElement.java
@@ -1,18 +1,16 @@
-/**
- * Solution
- */
-public class Solution {
+package CodingQuestions;
 
+public class OccurrenceOfElement {
     public static void main(String[] args) {
         int[] arr = {1,1,1,1,1,2,3,4,7,8,3,4,5,2,2,4,9,8};
-
+        
         occurrenceOfElement(arr);
     }
 
     static void occurrenceOfElement(int[] arr){
         sortArrays(arr);
 
-        for (int i = 0; i < arr.length -1; i++) {
+        for (int i = 0; i < arr.length-1; i++) {
             int count = 1;
             for (int j = i + 1; j < arr.length; j++) {
                 if(arr[i] == arr[j]){
@@ -22,24 +20,25 @@ public class Solution {
                     break;
                 }
             }
-
-            System.out.println(arr[i] + " | " + count);
-            i+= (count-1);
+            System.out.println(arr[i] +  " | " + count);
+            i+=(count-1);
+            
         }
     }
 
     static void sortArrays(int[] arr){
         for (int i = 0; i < arr.length -1; i++) {
-            int minimum = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if(arr[minimum] > arr[j]){
-                    minimum = j;
+            int smallest = i;
+            for (int j = i +1; j < arr.length; j++) {
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
                 }
             }
 
-            int temp = arr[minimum];
-            arr[minimum] = arr[i];
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
             arr[i] = temp;
+            
         }
     }
 }
