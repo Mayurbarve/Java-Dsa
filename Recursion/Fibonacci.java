7efd84af76d64a4b708ab36b6a92a989f6b283fb
Mayurbarve <mayurbarve04@gmail.com>
2025-01-09 23:33:10 +0530

    update

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -1,25 +1,32 @@
-import java.util.Scanner;
 
 public class Fibonacci {
 
-    static void printFibo(int a, int b, int n) {
-        if (n == 0) {
-            return;
+    // static void printFibo(int a, int b, int n) {
+    //     if (n == 0) {
+    //         return;
+    //     }
+    //     int c = a + b;
+    //     System.out.print(c + " ");
+    //     printFibo(b, c, n - 1);
+    // }
+    static int fibo(int n){
+        if(n < 2){
+            return n;
         }
-        int c = a + b;
-        System.out.println(c);
-        printFibo(b, c, n - 1);
+
+        return fibo(n - 1) + fibo(n - 2);
     }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter nth Number: ");
-        int n = in.nextInt();
-        int a = 0, b = 1;
-        System.out.println(a);
-        System.out.println(b);
+        // Scanner in = new Scanner(System.in);
+        // System.out.print("Enter nth Number: ");
+        // int n = in.nextInt();
+        // int a = 0, b = 1;
+        // System.out.print(a + " ");
+        // System.out.print(b + " ");
 
-        printFibo(a, b, n - 2);
-        in.close();
+        // printFibo(a, b, n - 2);
+        // in.close();
+        System.out.print(fibo(3));
     }
 }
